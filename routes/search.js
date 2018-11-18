9bce55ba5b4f171cd9e95f6c35e29e469af92502
Flavia Masoni <flavia.masoni@gmail.com>
2018-11-18 15:48:32 +0100

    add search page

---
+++ routes/search.js
@@ -0,0 +1,9 @@
+var express = require("express");
+var router = express.Router();
+
+/* GET search page. */
+router.get("/", function(req, res, next) {
+  res.render("search", { title: "Express" }); //mostra una pagina HTML, cerca search nella /views e lo mostra
+});
+
+module.exports = router;
