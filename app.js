c40a71b3018997fd5977fa9cbf6d1c9ed42efbbc
Flavia Masoni <flavia.masoni@gmail.com>
2018-11-19 19:52:36 +0100

    Added photo to buttons, changed searchbar style, linked every page to home

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.use("/search",searchRouter);
 app.use("/AboutUs",AboutUsRouter);
 app.use("/SignUp",SignUpRouter);
 app.use("/SignIn",SignInRouter);
-app.use("/searchCocktails",searchCocktails);
+app.use("/searchCocktail",searchCocktails);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
