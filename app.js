34b060bbcb109d24cacc6375ad9fcdfd4a140567
Flavia Masoni <flavia.masoni@gmail.com>
2018-11-22 08:08:03 +0100

    API implemented

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
-
+const request = require('request');
+const apikey = "9f30fd6c33e0ab0ffcd0d2f1ae5c7099";
+const appid = "e6ad82b4";
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
@@ -35,7 +37,23 @@ app.use("/SignIn",SignInRouter);
 app.use("/searchCocktail",searchCocktails);
 app.use("/results",results);
 
+app.get('/',function(req,res) {
+  res.render('/search',{recipe: null, error:null})
+});
 
+app.post('/',function(req,res){
+  let name = req.body.name_field;
+  let url ='https://api.edamam.com/search?q='+name+'&app_id='+appid+'&app_key='+apikey;
+   request(url,function(err,response,body){
+     if(err){
+       res.render('search',{recipe:null,error:'error, try with another ingredient'})
+     }
+     else {
+       res.json(response); //vede se la richiesta è andata a buon fine
+                          //res.render fa il rendere, res.json fa il render del json
+     }
+   })
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/search.js
+++ routes/search.js
@@ -1,28 +1,9 @@
 var express = require("express");
 var router = express.Router();
-const apikey = "9f30fd6c33e0ab0ffcd0d2f1ae5c7099";
-const appid = "e6ad82b4";
+
 /* GET search page. */
 router.get("/", function(req, res, next) {
   res.render("search", { title: "Express" }); //mostra una pagina HTML, cerca search nella /views e lo mostra
 });
 
 module.exports = router;
-app.get('/',function(req,res) {
-  res.render('/search',{recipe: null, error:null})
-});
-
-app.post('/',function(req,res){
-  let name = req.body.name_field;
-  let url ='https://api.edamam.com/search?q='+name+'&app_id='+appid+'&app_key='+apikey;
-   request(url,function(err,response,body){
-     if(err){
-       res.render('search',{recipe:null,error:'error, try with another ingredient'})
-     }
-     else {
-       let rec;
-     }
-
-   })
-
-});
