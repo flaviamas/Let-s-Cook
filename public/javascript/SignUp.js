115f6ca9ace1ac69b818df22033098cfd948e89f
ilaria <i.ponzianelli@gmail.com>
2018-12-08 23:23:13 +0100

    validaForm SignUp

--- public/javascript/SignUp.js
+++ public/javascript/SignUp.js
@@ -1,13 +1,13 @@
 
 function validaForm(){
 
-    var email_reg_exp= /^([a-zA-Z0-9_.-])+@(([a-zA-Z0-9-]{2,})+.)+([a-zA-Z0-9]{2,})+$/;
+   /*var email_reg_exp= /^([a-zA-Z0-9_.-])+@(([a-zA-Z0-9-]{2,})+.)+([a-zA-Z0-9]{2,})+$/;
   
     if (!email_reg_exp.test(emailid) || (emailid == "") || (emailid == "undefined")) {
       alert("Please, enter correct email address!");
       document.sign_up.emailid.select();
       return false;
-    }
+    }*/
   
     if(document.sign_up.password.value != document.sign_up.cpassword.value){
       alert("Different passwords, please enter the same password!");
