031b10655f8fd30cfd3f0db3b3f72d4f895690b4
Flavia Masoni <flavia_ma@hotmail.it>
2019-09-19 12:03:12 +0200

    change cocktail image and add mongo DB

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,26 @@
 var express = require("express");
 var router = express.Router();
-var pgp = require('pg-promise')();
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+
+// Connection URL
+const url = 'mongodb://localhost:27017';
+
+// Database Name
+const dbName = 'Letscook';
+
+// Create a new MongoClient
+const client = new MongoClient(url);
+
+// Use connect method to connect to the Server
+client.connect(function(err) {
+  assert.equal(null, err);
+  console.log("Connected successfully to server");
+
+  const db = client.db(dbName);
+
+  client.close();
+});
 
 
 /* GET home page. */
@@ -8,7 +28,7 @@ router.get("/", function(req, res, next) {
   res.render("index", { title: "Express" });
 });
 
-
+/*
 //controllo campo login
 router.post("/", function(req, res) {
   var email= req.body.email_login;
@@ -29,7 +49,7 @@ router.post("/", function(req, res) {
     console.log("LOGIN ERROR!!!!!!!!!!!!!")
     res.render("SignUp",);
   });
-});
+});*/
 
 
 module.exports = router;
