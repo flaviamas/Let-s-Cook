17a080e5ad9f1740007768817686fedd962e003f
Flavia Masoni <flavia_ma@hotmail.it>
2019-09-24 18:39:58 +0200

    back to postgres

--- routes/index.js
+++ routes/index.js
@@ -1,26 +1,8 @@
 var express = require("express");
 var router = express.Router();
-const MongoClient = require('mongodb').MongoClient;
+var pgp = require('pg-promise')();
 const assert = require('assert');
 
-// Connection URL
-const url = 'mongodb://localhost:27017';
-
-// Database Name
-const dbName = 'Letscook';
-
-// Create a new MongoClient
-const client = new MongoClient(url);
-
-// Use connect method to connect to the Server
-client.connect(function(err) {
-  assert.equal(null, err);
-  console.log("Connected successfully to server");
-
-  const db = client.db(dbName);
-
-  client.close();
-});
 
 
 /* GET home page. */
@@ -28,7 +10,7 @@ router.get("/", function(req, res, next) {
   res.render("index", { title: "Express" });
 });
 
-/*
+
 //controllo campo login
 router.post("/", function(req, res) {
   var email= req.body.email_login;
@@ -49,7 +31,7 @@ router.post("/", function(req, res) {
     console.log("LOGIN ERROR!!!!!!!!!!!!!")
     res.render("SignUp",);
   });
-});*/
+});
 
 
 module.exports = router;
